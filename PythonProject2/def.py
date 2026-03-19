9a23c5d9d2437ab0394471747247bdb2650e6aea
Adam57055 <adamtwam85@gmail.com>
2026-03-19 12:11:58 -0400

    add comments for clarity, round answers to two decimal places, and change to 'while true' function

--- PythonProject2/def.py
+++ PythonProject2/def.py
@@ -1,18 +1,24 @@
-num1 = int(input('Enter the first number:'))
-num2 = int(input('Enter the second number:'))
-operator = input('Enter an operator:')
-if operator == '+':
-    print(num1+num2)
-elif operator == '-':
-    print(num1-num2)
-elif operator == '*':
-    print(num1*num2)
-elif operator == '/':
-    if num2 != 0:
-        print(num1/num2)
+#Calculator
+while True:
+    num1 = float(input('Enter the first number:'))
+    num2 = float(input('Enter the second number:'))
+    operator = input('Enter an operator:')
+    if operator == '+':
+        addition = num1+num2
+        print(round(addition,2))
+    elif operator == '-':
+        subtraction = num1-num2
+        print(round(subtraction,2))
+    elif operator == '*':
+        multiplication = num1*num2
+        print(round(multiplication,2))
+    elif operator == '/': #denominator cannot be zero
+        if num2 != 0:
+            division = num1/num2
+            print(round(division,2))
+        else:
+            print('invalid number')
+            continue #program reprompts user
     else:
-        print('invalid number')
-else:
-    print('INVALID OPERATOR!')
-
-
+        print('INVALID OPERATOR!')
+        continue #program reprompts user
